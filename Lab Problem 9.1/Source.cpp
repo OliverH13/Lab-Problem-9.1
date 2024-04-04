dcbcafa89ac2d8fa448f4cd4a8e01cf30bd5536a
Oliver Hendricks <olliehendricks04@gmail.com>
2024-04-04 09:35:10 -0400

    Can now display every element in order of appearance in the array.

--- Lab Problem 9.1/Source.cpp
+++ Lab Problem 9.1/Source.cpp
@@ -5,8 +5,16 @@ using namespace std;
 
 int main() {
 
+	srand(time(NULL));
+	int random_integers[10];
 
+	for (int x = 0; x < 10; x++) {
+		random_integers[x] = rand() % 9000 + 1000;
+	}
+	
+	cout << "The random integers: ";
 
+	for (int x = 0; x < 10; x++) {
+		cout << random_integers[x] << "  ";
+	}
 }
-
-
