946437a8d1c8d8414b815af87e56b5d93d635675
Oliver Hendricks <olliehendricks04@gmail.com>
2024-04-04 09:50:46 -0400

    Can now display every even element

--- Lab Problem 9.1/Source.cpp
+++ Lab Problem 9.1/Source.cpp
@@ -9,7 +9,7 @@ int main() {
 	srand(time(NULL));
 	int random_integers[10];
 
-	// "Randomness" for the array
+	// "Randomness" for the array and output of only four digits
 	for (int x = 0; x < 10; x++) {
 		random_integers[x] = rand() % 9000 + 1000;
 	}
@@ -25,5 +25,11 @@ int main() {
 	cout << "Even indicies:       ";
 	cout << random_integers[1] << "  " << random_integers[3] << "  " << random_integers[5] << "  " << random_integers[7] << "  " << random_integers[9] << endl;
 
+	// Every even element in the array
+	cout << "Even values:         ";
+	for (int x = 0; x < 10; x++) {
+		if(random_integers[x] % 2 == 0) cout << random_integers[x] << "  ";
+	}
+	cout << endl;
 
 }
